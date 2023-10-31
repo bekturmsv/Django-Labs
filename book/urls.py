e5e85b1b3356ae257bea32cfd3f5b5fffd785f82
bekturmsv <itsbektur@gmail.com>
2023-10-31 19:55:25 +0600

    Lab finished,CRUD on class components, added a search option

--- book/urls.py
+++ book/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.view_book),
-    path('book_detail/<int:id>/', views.view_book_detail),
-    path('book_list/', views.view_book_delete),
-    path('book_list/<int:id>/delete/', views.view_book_drop),
-    path('create_book/', views.createViewBookPost),
-    path('add-comment/', views.createReviewForBook),
+    # path('', views.view_book),
+    # path('book_detail/<int:id>/', views.view_book_detail),
+    # path('book_list/', views.view_book_delete),
+    # path('book_list/<int:id>/delete/', views.view_book_drop),
+    # path('create_book/', views.createViewBookPost),
+    # path('add-comment/', views.createReviewForBook),
+    path("", views.BookView.as_view()),
+    path('book_detail/<int:id>/', views.BookDetailView.as_view()),
+    path('book_list/<int:id>/delete/', views.BookDropView.as_view()),
+    path('book_list/<int:id>/update/', views.UpdateBookView.as_view()),
+    path('create_post_book/', views.CreateBookView.as_view()),
+    path('add-comment/', views.createBookReview),
+    path('search/', views.SearchView.as_view(), name='search'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,48 +1,118 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from . import models, forms
-
+from django.views import generic
 
 
 # Create your views here.
 
-def view_book(request):
+class BookView(generic.ListView):
     book_value = models.Book.objects.all()
-    return render(request,"books/books.html",{"book_key" : book_value})
+    template_name = "books/book_list.html"
 
-def view_book_detail(request, id):
-    book_id = get_object_or_404(models.Book, id = id)
-    return render(request, 'books/book_detail.html', {'book_key' : book_id})
+    def get_queryset(selfs):
+        return models.Book.objects.all()
 
-def createViewBookPost(request):
+# def view_book(request):
+#     book_value = models.Book.objects.all()
+#     return render(request,"books/books.html",{"book_key" : book_value})
+
+class BookDetailView(generic.DetailView):
+    template_name = "books/book_detail.html"
+
+    def get_object(self):
+        book_id = self.kwargs.get("id")
+        return get_object_or_404(models.Book, id=book_id)
+
+# def view_book_detail(request, id):
+#     book_id = get_object_or_404(models.Book, id = id)
+#     return render(request, 'books/book_detail.html', {'book_key' : book_id})
+
+class CreateBookView(generic.CreateView):
+    template_name = "books/create_book.html"
+    form_class = forms.BookForm
+    queryset = models.Book.objects.all()
+    success_url = "/"
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(CreateBookView, self).form_valid(form=form)
+
+# def createViewBookPost(request):
+#     method = request.method
+#     if method == "POST":
+#         form = forms.BookForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Успешно добавлен')
+#     else:
+#         form = forms.BookForm()
+#     return render(request, 'books/create_book.html', {'form': form})
+
+
+class UpdateBookView(generic.UpdateView):
+    template_name = "books/update_book.html"
+    form_class = forms.BookForm
+    success_url = "/"
+
+    def get_object(self, **kwargs):
+        book_id = self.kwargs.get("id")
+        return get_object_or_404(models.Book, id=book_id)
+
+    def form_valid(self, form):
+        return super(UpdateBookView, self).form_valid(form=form)
+
+# def view_book_delete(request):
+#     lang_value = models.Book.objects.all()
+#     return render(request, 'books/book_list.html', {'lang_key': lang_value})
+
+
+class BookDropView(generic.DeleteView):
+    template_name = "books/confirm_book_delete.html"
+    success_url = "/"
+
+    def get_object(self, queryset=None):
+        book_id = self.kwargs.get("id")
+        return get_object_or_404(models.Book, id=book_id)
+
+def createBookReview(request):
     method = request.method
-    if method == "POST":
-        form = forms.BookForm(request.POST, request.FILES)
+    if(method == "POST"):
+        form = forms.ReviewForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponse('Успешно добавлен')
-    else:
-        form = forms.BookForm()
-    return render(request, 'books/create_book.html', {'form': form})
-
-def view_book_delete(request):
-    lang_value = models.Book.objects.all()
-    return render(request, 'books/book_list.html', {'lang_key': lang_value})
-
-def view_book_drop(request, id):
-    lang_id = get_object_or_404(models.Book, id=id)
-    lang_id.delete()
-    return HttpResponse('Успешно удален')
-
-def createReviewForBook(requset):
-    method = requset.method
-    if method == 'POST':
-        form = forms.ReviewForm(requset.POST, requset.FILES)
-        if form.is_valid():
-            form.save()
-
-            return HttpResponse('<h1>Success</h1>')
+            return HttpResponse("<h1>Успешно добавлен</h1>")
     else:
         form = forms.ReviewForm()
 
-    return render(requset, 'books/createReview.html', {'form' : form})
+    return render(request, "books/createReview.html",{"form": form})
+
+class SearchView(generic.ListView):
+    template_name = "books/book_list.html"
+    context_object_name = "book"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return models.Book.objects.filter(title__icontains=self.request.GET.get("q"))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get("q")
+        return context
+# def view_book_drop(request, id):
+#     lang_id = get_object_or_404(models.Book, id=id)
+#     lang_id.delete()
+#     return HttpResponse('Успешно удален')
+#
+# def createReviewForBook(requset):
+#     method = requset.method
+#     if method == 'POST':
+#         form = forms.ReviewForm(requset.POST, requset.FILES)
+#         if form.is_valid():
+#             form.save()
+#
+#             return HttpResponse('<h1>Success</h1>')
+#     else:
+#         form = forms.ReviewForm()
+#
+#     return render(requset, 'books/createReview.html', {'form' : form})
